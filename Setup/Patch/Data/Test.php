4655201e5d7fb79f4e1f3b969299c521d9b6a241
Jeff Britts <jbritts@adobe.com>
2020-03-23 09:50:29 -0400

    added test data

---
+++ Setup/Patch/Data/Test.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace MagentoEse\VeniaTest\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use MagentoEse\DataInstall\Model\Process;
+
+class Test implements DataPatchInterface
+{
+    /** @var Process  */
+    protected $process;
+
+    public function __construct(Process $process)
+    {
+        $this->process = $process;
+    }
+
+    public function apply()
+    {
+//        $this->process->loadFiles(['MagentoEse_VeniaTest::fixtures/categories.csv','MagentoEse_VeniaTest::fixtures/stores.csv',
+//            'MagentoEse_VeniaTest::fixtures/product_attributes.csv']);
+        $this->process->loadFiles(['MagentoEse_VeniaTest::fixtures/categories.csv']);
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
